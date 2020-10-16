092dfa1c1bfc70c5bf0a50118a1edf7588778d9a
spawn18 <spawn18@protonmail.com>
2020-10-16 23:25:51 +0500

    added username/password method implementation

--- protocol/methods/userpass.c
+++ protocol/methods/userpass.c
@@ -9,135 +9,55 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAX_USERS_RECORD_SIZE 528
+#define AUTH_LEN 513
 
-static FILE* usersFile = NULL;
 
-typedef struct user_
-{
-    char* username;
-    char* password;
-} user;
-
-/**
- * @brief Handle username/password authentication
- * @note Hardcoded to use as less recv system calls as possible
- *
- * @param client Socket to get message from
- *
- * @return TRUE if succeed, FALSE otherwise
- */
 bool handle_method_userpass(fd_t client)
 {
-    /* Version and username length*/
-    char ver_and_usrname_len[2];
-    recv_all(client, ver_and_usrname_len, 2);
+    char auth[AUTH_LEN + 1];
+    recv_all(client, auth, 2);
 
-    if(ver_and_usrname_len[0] == SOCKS_VERSION)
+    bool success = FALSE;
+    if(auth[0] == SOCKS_VER)
     {
-        /* Get username */
-        char usrname_and_pass_len[2];
-        recvf(client, usrname_and_pass_len, 2);
+        recv_all(client, auth+2, auth[1]+1);
+        char* user = auth+2;
+        char userLen = auth[1];
 
-        /* Get password */
-        char password[usrname_and_pass_len[1]];
-        recvf(client, password, usrname_and_pass_len[1]);
+        recv_all(client, auth+3+userLen, auth[2+userLen]);
+        char* pass = auth+3+userLen;
+        char passLen = auth[2+userLen];
 
-        /* Verify Username/Password via database and proceed */
-        if(usersFile == NULL)
+
+        FILE* file = fopen("users.txt", "r");
+
+        if(file != NULL)
         {
-            FILE* users = fopen("users.txt", "r");
-        }
+            char entry[512];
 
-        user u;
-        char* username = &usrname_and_pass_len[0];
-        int userCounter = 1;
-        bool userFound = FALSE;
-
-        do
-        {
-            get_next_user(usersFile, u);
-            userFound = strcmp(username, u.username) != 0 && strcmp(password, u.password) != 0
-        }
-        while(!userFound);
-
-        if(userFound)
-        {
-            return TRUE;
-        }
-        else
-        {
-
-        }
-    }
-    else
-    {
-        LOG_MSG("INCORRECT PROTOCOL IN METHOD AUTHENTICATION");
-    }
-
-    return FALSE;
-}
-
-
-bool get_next_user(FILE *users, user u)
-{
-    char* entry;
-    size_t size;
-
-    getline(&entry, &size, users);
-
-    char cleanEntry[size];
-    remove_char(entry, cleanEntry, ' ');
-
-    free(entry);
-
-    // Correct size
-    if(size <= MAX_USERS_RECORD_SIZE + 2)
-    {
-        // Correct beginning and end
-        if(cleanEntry[0] == '{' && cleanEntry[size-1] == '}')
-        {
-            // Username check
-            char usernameLen = 0;
-            if(strncmp(&cleanEntry[1], "username:", 9) == 0)
+            while(fgets(entry, 512, file) != NULL)
             {
-                char* username_p = &cleanEntry[10];
-
-                for(int i = 0; i < 256; i++)
+                if(strncmp(entry[0], user, userLen) == 0)
                 {
-                    usernameLen++;
-                    if(*username_p++ == ',') break;
-                }
-
-                // Username correct
-                if(*username_p == ',')
-                {
-                    strncpy(u.username, username_p, usernameLen);
-
-                    // Password check
-                    if(strncmp(&cleanEntry[10+usernameLen], "password:", 9) == 0)
+                    if(strncmp(entry[userLen+1], pass, passLen) == 0)
                     {
-                        char* password_p = &cleanEntry[20+usernameLen];
-                        char  passwordLen = 0;
-
-                        for(int i = 0; i < 256; i++)
-                        {
-                            if(*password_p++ == '}') break;
-                        }
-
-                        // Password correct
-                        if(*password_p == '}')
-                        {
-                            strncpy(u.password, password_p, passwordLen);
-
-                            return TRUE;
-                        }
+                        success = TRUE;
+                        break;
                     }
                 }
             }
+
+            fclose(file);
         }
     }
 
-    return FALSE;
+    char code = success ? 0 : 1;
+    char rep[2] = {SOCKS_VER, code};
+    send_all(client, rep, 2);
+
+    return success;
 }
+
+
+
 
