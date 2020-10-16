d46b2b3d8b42e0eb7cc89ad2b28788d3ae668952
spawn18 <spawn18@protonmail.com>
2020-10-17 00:04:00 +0500

    added username/password method implementation

--- main.c
+++ main.c
@@ -1,25 +1,11 @@
-#include <stdlib.h>
 #include "system/args.h"
 #include "system/server.h"
 
-#include "system/log.h"
-
-void clean_up()
-{
-    if(is_log_open())
-    {
-        log_save();
-        log_close();
-    }
-}
 
 int main(int argc, char** argv)
 {
-    if(atexit(clean_up) == 0)
-    {
-        handle_args(argc, argv);
-        serve();
-    }
+    handle_args(argc, argv);
+    serve();
 
     return 0;
 }
--- system/options.c
+++ system/options.c
@@ -5,7 +5,7 @@ int METHOD_PREFERED = METHOD_NOAUTH;
 int MAX_CONNECTIONS = 1024;
 unsigned short PORT = 1080;
 
-static unsigned int FLAGS = 0x0;
+static unsigned int FLAGS = 0;
 
 bool is_opt_set(option_t opt)
 {
