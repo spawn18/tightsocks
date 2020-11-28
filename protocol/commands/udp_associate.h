a718e6028ed13b7752e66e5ccaa6a212c70583e0
spawn18 <spawn18@protonmail.com>
2020-11-28 13:02:25 +0500

    meh

--- protocol/commands/udp_associate.h
+++ protocol/commands/udp_associate.h
@@ -5,4 +5,14 @@
 
 void SOCKS_udp_associate(sock_t, atyp_t, char*, char*);
 
+typedef struct UDP_DGRAM_T
+{
+	char RSV[2];
+	char FRAG;
+	char ATYP;
+	char DSTADDR[255];
+	char DSTPORT[2];
+	char DATA[65535];
+};
+
 #endif
